cea4c3b2b46da798800754073ff12bd72ff83f84
Adam Gospodarczyk <adam@overment.com>
2020-04-17 19:17:54 +0200

    02-finish

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Variable types
+let counter;
+let text;
+let logic;
+text = 'Lorem ipsum';
+// Arrays
+let list = [1, 2, 3];
+let list2 = [1, 2, 3];
+// list.push('test');
+// Functions
+const play = function (song) {
+    console.log(`Playing ${song}`);
+    return true;
+};
+// play({ name: 'Unforgiven '});
+play('Unforgiven');
