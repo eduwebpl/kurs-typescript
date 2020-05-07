cf648ed72f3329c91d17e29a473354d8e9eba53b
Adam Gospodarczyk <adam@overment.com>
2020-05-07 12:05:46 +0200

    18

--- app.ts
+++ app.ts
@@ -1,20 +1,3 @@
-// Function Constructor Syntax
-function Movie(title) {
-  this.title = title
-}
-
-Movie.prototype.getTitle = function () {
-  return this.title
-}
-
-// Movie() /*?*/
-const bestMovieEver = new Movie("Peaceful Warrior") /*?*/
-bestMovieEver /*?*/
-
-bestMovieEver.getTitle() /*?*/
-
-// Class Syntax
-
 class Movie {
   constructor(private title: string) {}
 
