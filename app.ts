f910331b5b6b161848d957077bce8bb274017bbc
Adam Gospodarczyk <adam@overment.com>
2020-05-11 09:45:44 +0200

    19

--- app.ts
+++ app.ts
@@ -1,25 +1,6 @@
-// public - anyone can access
-// private - available only inside a class
-// protected - available inside a class and subclasses
-// readonly - property can't be modified
-// static - is available directly from class (without instantiation)
 class Movie {
-  genre: string
-
-  constructor(private readonly title: string, genre: string) {
-    this.genre = genre
-  }
-
-  getTitle(): string {
-    return this.title
-  }
+  constructor(private _title: string) {}
 }
 
-const bestMovieEver = new Movie("Peaceful Warrior", "Drama")
-bestMovieEver /*?*/
-bestMovieEver.genre /*?*/
-bestMovieEver.getTitle() /*?*/
-
-bestMovieEver.getTitle() /*?*/
-
-Movie.type /*?*/
+const bestMovieEver = new Movie("Peaceful Warior")
+bestMovieEver._title /*?*/
