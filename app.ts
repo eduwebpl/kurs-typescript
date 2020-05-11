02cc796938595727a674fba733b764f51dcf6cfd
Adam Gospodarczyk <adam@overment.com>
2020-05-11 09:46:09 +0200

    19-finish

--- app.ts
+++ app.ts
@@ -1,6 +1,15 @@
 class Movie {
   constructor(private _title: string) {}
+
+  private set title(title: string) {
+    this._title = `${title}!`
+  }
+
+  private get title() {
+    return this._title.toUpperCase()
+  }
 }
 
 const bestMovieEver = new Movie("Peaceful Warior")
-bestMovieEver._title /*?*/
+bestMovieEver.title = "Peaceful Warrior" // setter
+bestMovieEver.title /*?*/ // getter
