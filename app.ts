66bf0dddc544a1f5e1dce7e6a2954b6413197a5f
Adam Gospodarczyk <adam@overment.com>
2020-05-12 10:28:56 +0200

    20-finish

--- app.ts
+++ app.ts
@@ -1,3 +1,34 @@
-class Movie {
-  constructor(private _title: string) {}
+class Media {
+  public progress = 0
+
+  constructor(
+    readonly name: string,
+    public readonly type: string,
+    public readonly genre: string,
+    public readonly duration: number,
+  ) {}
+
+  play() {
+    this.progress += 1
+  }
 }
+
+class Movie extends Media {
+  constructor(name: string, genre: string, duration: number) {
+    super(name, "movie", genre, duration)
+  }
+}
+
+class Song extends Media {
+  constructor(name: string, genre: string, duration: number) {
+    super(name, "song", genre, duration)
+  }
+}
+
+const bestMovieEver = new Movie("Peaceful Warior", "Drama", 7200)
+bestMovieEver.play()
+bestMovieEver.progress /*?*/
+const bestSongEver = new Song("Heart of Courage", "Cinematic", 117)
+bestSongEver.type /*?*/
+bestSongEver.play()
+bestSongEver.progress /*?*/
